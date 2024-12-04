50bac478883d73d07780c73c2f5b0c97605e1e57
HazemElbehary <hazemelbehary19@gmail.com>
2024-12-04 11:11:56 +0200

    Add project files.

---
+++ SignalRProject/DbContextFolder/ChatDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SignalRProject.Models;
+
+namespace SignalRProject.DbContextFolder
+{
+	public class ChatDbContext: DbContext
+	{
+        public ChatDbContext(DbContextOptions<ChatDbContext> options): base(options)
+        {
+        }
+        public DbSet<Message> Message { get; set; }
+    }
+}
---
+++ SignalRProject/HubFolder/ChatHub.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalRProject.DbContextFolder;
+using SignalRProject.Models;
+
+namespace SignalRProject.HubFolder
+{
+    public class ChatHub(ChatDbContext dbContext): Hub
+    {
+        public async void Send(string userName, string message)
+        {
+            // 1. Save Message In DB
+            Message msm = new Message() 
+            {
+                UserName = userName,
+                Text = message 
+            };
+            await dbContext.Message.AddAsync(msm);
+            dbContext.SaveChanges();
+
+            // 2. Call JS Function That Fire Action in JS
+            //    to Call All Receive MSM Fucntion "that Send For ALl Users This MSM"
+            await Clients.Others.SendAsync("ReceiveMessage", userName, message);
+        }
+    
+        public async void JoinGroup(string groupName, string userName)
+        {
+            // 1. Add All Users With ConnectionId to the Same Group
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            // 2. Call Method Send In Group "New Memeber Join"
+            await Clients.OthersInGroup(groupName).SendAsync("NewMemberJoin", userName, groupName);
+        }
+    
+        public async void SendToGroup(string groupName, string userName, string groupMessage)
+        {
+            // 1. Save Message In DB
+            Message msm = new Message()
+            {
+                UserName = userName,
+                Text = groupMessage
+            };
+            await dbContext.Message.AddAsync(msm);
+            dbContext.SaveChanges();
+
+
+            // 2. Call JS Function That Fire Action in JS
+            //    to Call All Receive MSM Fucntion "that Send For ALl Users This MSM"
+            await Clients.OthersInGroup(groupName).SendAsync("ReciveMessageFromGroup", userName, groupMessage);
+        }
+    }
+}
